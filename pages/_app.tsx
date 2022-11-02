e8303b99da98c8c21eabcda00fe19891a9e39c34
0xArb <arb@subtract.fi>
2022-11-02 23:05:20 +0100

    plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import "../styles.css";
+import PlausibleProvider from "next-plausible";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <PlausibleProvider domain="subtract.fi">
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  );
 }
 
 export default MyApp;
